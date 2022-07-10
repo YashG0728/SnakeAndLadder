abdef678212785fce8c3bbf57c7d7e7cf7e333d3
YashG0728 <yashgharde07@gmail.com>
2022-07-10 21:44:59 +0530

    initial commit

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+				
+	System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
+				
+	int startPositionA = 0;
+	}
+}
